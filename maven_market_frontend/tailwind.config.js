ff3b6d40a0de0847eb5ac587e539a0e81da74e59
noman ajmal <noman@fourmangos.com>
2022-10-28 21:56:44 +0500

    configured the theme and added hot reloding

--- maven_market_frontend/tailwind.config.js
+++ maven_market_frontend/tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '786px',
+      lg: '986px',
+      xl: "1440px"
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: { 
+      colors: {
+      backGroudGray: "hsl(220,14%,96%)",
+      green: "hsl(159,99%,41%)",
+      black : "black",
+      grey: "hsl(227,10%,64%)",
+      blue: "hsl(214,100%,55%)"
+    },
+    fontFamily:{
+      railway = ['Raleway', 'sans-serif'],
+      imFellFrenchCannon = ['IM Fell French Canon', 'serif'],
+      Andika = ['Andika', 'sans-serif']
+    }
+  
+  }
   },
   plugins: [],
 }
