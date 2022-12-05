e1859c2cfd6d9a1d894e68771fae1d5d017a4389
hassan ajmal <hassanajmal531@gmail.com>
2022-12-06 02:26:50 +0500

    added the twitter detection

---
+++ Services/twitter/api.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import requests
+import threading
+
+from script import script
+app = Flask(__name__)
+string = "asd"
+
+def call(url):
+    
+    
+    data = script(url)
+    
+    if (data == 0):
+    # url is of the mock server in postman
+    
+        requests.post("https://fbfb19c3-7109-4063-a0f1-636df130674c.mock.pstmn.io", json={"activateStatus": "1"})
+    if (data == 1):
+# url is of the mock server in postman
+
+        requests.post("https://fbfb19c3-7109-4063-a0f1-636df130674c.mock.pstmn.io", json={"activateStatus": "0"})
+
+@app.route('/', methods = ["POST"])
+def method_name():
+    data = request.json
+    thread = threading.Thread(target=call, args=(data["url"],))
+    thread.start()
+    return "ok", 200
+
+app.run()
