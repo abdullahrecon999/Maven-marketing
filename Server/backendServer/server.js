0de463bc3cd814d4506c38398a974e17fd4f2871
abdullah <abdullahrecon999@gmail.com>
2023-02-10 01:09:54 +0500

    ADD: added dockerfile

--- Server/backendServer/server.js
+++ Server/backendServer/server.js
@@ -7,7 +7,8 @@ const DB = process.env.MONGOURI;
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
     })
     .then(() => console.log('[+] DB Connected'));
 
