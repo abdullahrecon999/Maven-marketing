9cc4659ea8f89b225de1cc386523f5da5045caec
hassan ajmal <hassanajmal531@gmail.com>
2023-02-28 22:03:31 +0500

    changed the platforms and other feature like it to array and also added search function

--- server/models/Campaign.js
+++ server/models/Campaign.js
@@ -8,27 +8,33 @@ const campaignSchema = new mongoose.Schema({
   },
   title: {
     type: String,
+    required: true
   },
   email: {
     type: String,
   },
-  langauge: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  language: {
-    type: String,
-  },
+  langauge: Array,
+  country: Array,
+  
   description: {
     type: String,
   },
-  platform: {
-    type: String,
-  }
+  platform: Array
 }, { timestamps: true });
 
 const Campaign = mongoose.model('Campaign', campaignSchema);
+
+campaignSchema.statics.search = function(searchTerm) {
+  const stringSearchFields = ['language', "country", "platform", "description", "title"]
+  
+  const query = {
+      $or: [
+          ...stringSearchFields.map(field => ({
+              [field]: new RegExp('^' + searchTerm, 'i')
+          }))
+      ]
+  };
+  return this.find(query);
+};
 
 module.exports = Campaign;
