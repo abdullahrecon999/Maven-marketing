ce77023914b133d882f4938df0ddd5feee7f1549
hassan ajmal <hassanajmal531@gmail.com>
2023-04-27 21:52:58 +0500

    added comments

--- frontend/src/Components/MarketAutomationComponents/CommentComponent.jsx
+++ frontend/src/Components/MarketAutomationComponents/CommentComponent.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { AttachFile } from "@mui/icons-material";
+import React, { useState } from "react";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import { Button } from "antd";
+const CommentComponent = ({ open, isOpen }) => {
+  console.log(isOpen, "ths is the caleussjf");
+  const [text, setText] = useState("Comment.....");
+  return (
+    <div className="w-[90%]">
+      <div className="flex flex-col bg-slate-100 w-full">
+        <div
+          className="text-xs p-1 w-full min-h-16 focus:outline-none"
+          contentEditable="true"
+          onFocus={() => {
+            setText("");
+          }}
+        >
+          {text}
+        </div>
+        <div className="flex flex-row-reverse  px-1">
+          <label htmlFor="commentFile">
+            <AttachFile className="text-xs"></AttachFile>
+          </label>
+          <label className="items-center">
+            <SentimentSatisfiedAltIcon className="text-xs"></SentimentSatisfiedAltIcon>
+          </label>
+          <input id="commentFile" type="file" className="hidden" />
+        </div>
+      </div>
 
-const CommentComponent = () => {
-  return <div>CommentComponent</div>;
+      <div className="flex flex-row-reverse space-x-2 mt-2 ">
+        <Button className="bg-blue ml-1 text-white">Post</Button>
+        <Button
+          onClick={() => {
+            open(!isOpen);
+            console.log("clicked");
+          }}
+        >
+          cancel
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default CommentComponent;
--- frontend/src/Components/MarketAutomationComponents/PostSideComments.jsx
+++ frontend/src/Components/MarketAutomationComponents/PostSideComments.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import ProfileImage from "../../images/profile.jpg";
+import CommentComponent from "./CommentComponent";
+
 const PostSideComments = () => {
   const [openReply, setOpenReply] = useState(false);
+  const [openNewComment, setOpenNewComment] = useState(false);
   return (
     <div className="hidden w-[30%] md:block">
-      <div className="bg-white w-full p-2">
+      <div className="bg-white w-full p-2 ">
         {/* commentslist */}
-        <div className="flex">
+        {/* individual comment */}
+        {/* this will go in a loop */}
+        <div className="flex p-1 hover:border rounded-md ">
           <img className="w-5 h-5 rounded-full" src={ProfileImage} alt="" />
           <div>
             <div className="flex flex-col justify-center ">
@@ -20,11 +25,40 @@ const PostSideComments = () => {
 
             <div className="mt-1 px-1">
               <div className="text-xs"> @Bryn can you share the source?</div>
-              <label className="text-xxs text-gray-400 hover:text-gray-600">
-                reply
-              </label>
+              {openReply ? (
+                <label
+                  onClick={() => setOpenReply(!openReply)}
+                  className="text-xxs text-gray-400 hover:text-gray-600"
+                >
+                  reply
+                </label>
+              ) : // <CommentComponent open={setOpenNewComment} isOpen={openReply} />
+              null}
             </div>
           </div>
+        </div>
+        <hr></hr>
+        <div className="flex space-y-2 flex-col justify-center mt-1 hover:border rounded-md p-1 space-x-1">
+          <div className="flex space-x-1">
+            <img className="w-5 h-5 rounded-full" src={ProfileImage} alt="" />
+            {!openNewComment ? null : (
+              <label
+                onClick={() => setOpenNewComment(!openNewComment)}
+                className="text-xs text-gray-400"
+              >
+                Add a comment...
+              </label>
+            )}
+          </div>
+
+          {openNewComment === true && (
+            <div className="flex flex-row-reverse">
+              <CommentComponent
+                open={setOpenNewComment}
+                isOpen={!openNewComment}
+              ></CommentComponent>
+            </div>
+          )}
         </div>
       </div>
     </div>
