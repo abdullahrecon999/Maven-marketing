08765677748d7f053721b89c88039e02a71e689b
hassan ajmal <hassanajmal531@gmail.com>
2023-05-21 22:44:51 +0500

    fixed the bid modal

--- frontend/src/ValidationSchemas/bidValidation.js
+++ frontend/src/ValidationSchemas/bidValidation.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 const bidSchema = yup.object().shape({
-    discription: yup.string().required("Cover Letter is required0"),
-    amount : yup.string().required("Amount is required").matches(/^\d+$/ , "Amount should be a number  "). min(5, "the amount should be atleast 5"),
+    discription: yup.string().required("Cover Letter is required"),
+    amount : yup.string().required("Amount is required").matches(/^\d+$/ , "Amount should be a number  "),
     answer1 : yup.string().required("please answer the question"),
     answer2 : yup.string().required("please answer the question"),
     answer3 : yup.string().required("please answer the question")
